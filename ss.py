d51bbedf1b36713a236ffb74c11f5a50e78ca691
Matthew Finlayson <mattbnfin@gmail.com>
2024-04-10 16:52:38 -0700

    improvements, error catching, help, features

--- ss.py
+++ ss.py
@@ -14,46 +14,53 @@ def main():
 
 
 def parse_args():
-    alias_help = "Paper or author ID or alias"
+    alias_help = "Paper or author ID or alias."
     parser = argparse.ArgumentParser()
     parser.set_defaults(func=hello)
     subparsers = parser.add_subparsers()
 
-    search_parser = subparsers.add_parser("search", help="Search for papers")
-    search_parser.add_argument("query", help="Search term")
+    search_parser = subparsers.add_parser("search", help="Search for papers.")
+    search_parser.add_argument("query", help="Search term.")
+    search_parser.add_argument("-n", type=int, default=None, help="Number of results.")
     search_parser.set_defaults(func=search)
 
-    dl_parser = subparsers.add_parser("dl", help="Download a paper")
+    dl_parser = subparsers.add_parser(
+        "dl", help="Download a paper and print path to stdout."
+    )
     dl_parser.add_argument("alias", help=alias_help)
     dl_parser.add_argument(
-        "--url", action="store_true", help="Print paper url to stdout"
+        "--url", action="store_true", help="Print paper url to stdout."
     )
+    dl_parser.add_argument(
+        "--outdir", default="~/papers", help="Where to save the file."
+    )
+    dl_parser.add_argument("--stdout", action="store_true", help="Send pdf to stdout.")
     dl_parser.set_defaults(func=dl)
 
     citations_parser = subparsers.add_parser(
-        "citations", help="Get the articles that cite a paper"
+        "citations", help="Get the articles that cite a paper."
     )
     citations_parser.add_argument("alias", help=alias_help)
     citations_parser.set_defaults(func=citations)
 
-    paper_parser = subparsers.add_parser("paper", help="Get info on a paper")
+    paper_parser = subparsers.add_parser("paper", help="Get info on a paper.")
     paper_parser.add_argument("alias", help=alias_help)
     paper_parser.add_argument(
-        "--fields", type=str, default=FIELDS, help="Which fields to get from the API"
+        "--fields", type=str, default=FIELDS, help="Which fields to get from the API."
     )
     paper_parser.set_defaults(func=paper)
 
-    identifier_parser = subparsers.add_parser("id", help="Get a paper identifier")
+    identifier_parser = subparsers.add_parser("id", help="Get a paper identifier.")
     identifier_parser.add_argument("alias", help=alias_help)
     identifier_parser.set_defaults(func=identifier)
 
-    author_parser = subparsers.add_parser("author", help="Get an author's papers")
+    author_parser = subparsers.add_parser("author", help="Get an author's papers.")
     author_parser.add_argument("alias", help=alias_help)
     author_parser.add_argument(
         "--fields",
         type=str,
         default="papers.year,papers.title,papers.authors",
-        help="Which fields to retrieve",
+        help="Which fields to retrieve.",
     )
     author_parser.set_defaults(func=author)
 
@@ -74,7 +81,7 @@ def paper(alias=None, fields=None, **_):
     sys.stdout.write(response.text)
 
 
-def search(query=None, **_):
+def search(query=None, n=None, **_):
     response = requests.get(
         os.path.join(URL, "paper/search"),
         params=dict(query=query, fields=FIELDS),
@@ -83,7 +90,7 @@ def search(query=None, **_):
     if response.ok and "data" in response.json():
         papers = response.json()["data"]
         save_alias(get_aliases(papers))
-        json.dump(list(map(flatten_paper, papers)), sys.stdout)
+        json.dump(list(map(flatten_paper, papers))[:n], sys.stdout)
     else:
         print(response.text, file=sys.stderr)
 
@@ -112,7 +119,7 @@ def citations(alias=None, **_):
     json.dump(list(map(flatten_paper, papers)), sys.stdout)
 
 
-def dl(alias=None, url=False, **_):
+def dl(alias=None, url=False, outdir=None, stdout=False, **_):
     paper_id = get_id(alias)
     response = requests.get(
         os.path.join(URL, "paper", paper_id),
@@ -121,21 +128,23 @@ def dl(alias=None, url=False, **_):
     ).json()
     bibtex_id = get_bibtex_id(response["citationStyles"]["bibtex"])
     save_alias({bibtex_id: paper_id})
-    pdf_path = os.path.expanduser(f"~/papers/{bibtex_id}.pdf")
+    pdf_path = os.path.join(os.path.expanduser(outdir), bibtex_id + ".pdf")
     if response["isOpenAccess"]:
         paper_url = response["openAccessPdf"]["url"]
     elif "ArXiv" in response["externalIds"]:
         paper_url = f"https://arxiv.org/pdf/{response['externalIds']['ArXiv']}"
     else:
         print("No paper url", file=sys.stderr)
-        return
+        exit(1)
     if url:
         print(paper_url)
-    elif os.path.exists(pdf_path):
+    elif os.path.exists(pdf_path) and not stdout:
         print("Already downloaded.", file=sys.stderr)
         print(pdf_path)
+    paper_response = requests.get(paper_url)
+    if stdout:
+        sys.stdout.buffer.write(paper_response.content)
     else:
-        paper_response = requests.get(paper_url)
         with open(pdf_path, "wb") as f:
             f.write(paper_response.content)
         print(pdf_path)
@@ -189,15 +198,23 @@ def get_bibtex_id(bibtex: str):
 
 
 def save_alias(new_alias):
-    with open(IDS, "r") as f:
-        aliases = json.load(f)
+    if not os.path.exists(IDS):
+        os.makedirs(os.path.dirname(IDS), exist_ok=True)
+        aliases = dict()
+    else:
+        with open(IDS, "r") as f:
+            aliases = json.load(f)
     with open(IDS, "w") as f:
         json.dump(aliases | new_alias, f)
 
 
 def get_id(alias):
-    with open(IDS) as f:
-        return json.load(f)[alias]
+    try:
+        with open(IDS) as f:
+            return json.load(f)[alias]
+    except (KeyError, FileNotFoundError):
+        print(f"ID {alias} not found", file=sys.stderr)
+        exit(1)
 
 
 if __name__ == "__main__":
